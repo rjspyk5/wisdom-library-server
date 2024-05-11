17f37d6906b4b94059465c12e9491c6724e64062
rjspyk5 <rjspyk5@gmail.com>
2024-05-11 12:40:20 +0600

    setup server for initial deploy and for get url

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ require("dotenv").config();
 
 // middleware setup
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "wisdom-library-1acce.web.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "wisdom-library-1acce.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omgilvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,15 +31,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
