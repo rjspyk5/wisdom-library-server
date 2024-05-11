38e1c901543637609d6491c258c57108f72402e2
rjspyk5 <rjspyk5@gmail.com>
2024-05-11 12:27:10 +0600

    basic server setup complte

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const express = require;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware setup
+app.use(express.json());
+app.use(cors());
+
+//api for testing purpose
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+});
